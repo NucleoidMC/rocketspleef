56d4141a7664692b7412e733cc5356ddf8af901a
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2021-05-15 11:55:36 -0400

    rainbow

--- src/main/java/supercoder79/rocketspleef/game/RsChunkGenerator.java
+++ src/main/java/supercoder79/rocketspleef/game/RsChunkGenerator.java
@@ -1,8 +1,10 @@
 package supercoder79.rocketspleef.game;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.noise.PerlinNoiseSampler;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.biome.source.BiomeAccess;
@@ -14,11 +16,14 @@ import xyz.nucleoid.plasmid.game.world.generator.GameChunkGenerator;
 
 import java.util.Collections;
 import java.util.Optional;
+import java.util.Random;
 
 public class RsChunkGenerator extends GameChunkGenerator {
+    private final PerlinNoiseSampler colorNoise;
 
     public RsChunkGenerator(MinecraftServer server) {
         super(createBiomeSource(server, BiomeKeys.PLAINS), new StructuresConfig(Optional.empty(), Collections.emptyMap()));
+        this.colorNoise = new PerlinNoiseSampler(new Random(server.getOverworld().getSeed()));
     }
 
     @Override
@@ -31,17 +36,36 @@ public class RsChunkGenerator extends GameChunkGenerator {
                 for (int y = 0; y < 256; y++) {
                     int manhattan = Math.abs(x / 2) + Math.abs(z / 2) + Math.abs((y - 64) / 2);
 
+                    double progress = (y - 31) / 66.0;
+
+                    progress += this.colorNoise.sample(x / 8.0, y / 8.0, z / 8.0, 0, 0) * 0.05;
+
+                    Block glass;
+                    if (progress < (1 / 6.0)) {
+                        glass = Blocks.RED_STAINED_GLASS;
+                    } else if (progress < (1 / 3.0)) {
+                        glass = Blocks.ORANGE_STAINED_GLASS;
+                    } else if (progress < (1 / 2.0)) {
+                        glass = Blocks.YELLOW_STAINED_GLASS;
+                    } else if (progress < (2 / 3.0)) {
+                        glass = Blocks.LIME_STAINED_GLASS;
+                    } else if (progress < (5 / 6.0)) {
+                        glass = Blocks.BLUE_STAINED_GLASS;
+                    } else {
+                        glass = Blocks.PURPLE_STAINED_GLASS;
+                    }
+
                     if (manhattan <= 16) {
                         if (manhattan >= 12) {
-                            world.setBlockState(new BlockPos(x, y, z), Blocks.GLASS.getDefaultState(), 3);
+                            world.setBlockState(new BlockPos(x, y, z), glass.getDefaultState(), 3);
                         }
 
                         if (y <= 64 && y % 4 == 0) {
-                            world.setBlockState(new BlockPos(x, y, z), Blocks.GLASS.getDefaultState(), 3);
+                            world.setBlockState(new BlockPos(x, y, z), glass.getDefaultState(), 3);
                         }
 
                         if (manhattan == 11 && ((x - 1) % 2 == 0 && (z - 1) % 2 == 0 && (y - 1) % 2 == 0)) {
-                            world.setBlockState(new BlockPos(x, y, z), Blocks.GLASS.getDefaultState(), 3);
+                            world.setBlockState(new BlockPos(x, y, z), glass .getDefaultState(), 3);
                         }
                     }
                 }
